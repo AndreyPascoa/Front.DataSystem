a28c4c3a46a06068258c9aeb95e3db1dcb33d025
AndreyPascoa <andrey.pascoa@gmail.com>
2026-01-14 10:25:43 -0300

    Arrumado acento no statusMapper

--- src/utils/statusMapper.ts
+++ src/utils/statusMapper.ts
@@ -3,11 +3,11 @@ import { TaskStatus } from "@/types/task";
 export const statusToId: Record<TaskStatus, number> = {
   Pendente: 1,
   EmProgresso: 2,
-  Concluída: 3,
+  Concluida: 3,
 };
 
 export const idToStatus: Record<number, TaskStatus> = {
   1: "Pendente",
   2: "EmProgresso",
-  3: "Concluída",
+  3: "Concluida",
 };
